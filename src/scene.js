d26b7e1c744cf736a9c04181a29dd7dc09efcd67
bohadi <bohadi@users.noreply.github.com>
2017-09-22 00:48:55 -0500

    simple animation

--- src/scene.js
+++ src/scene.js
@@ -14,28 +14,69 @@ console.log();
 
 var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 
-var sphere1 = BABYLON.Mesh.CreateSphere('sphere1', 2, 2, scene);
+var sphere1 = BABYLON.MeshBuilder.CreateSphere('sphere1', {segments:2, diameter:2}, scene);
 sphere1.position.y = 3;
 sphere1.convertToFlatShadedMesh();
-var sphere2 = BABYLON.Mesh.CreateSphere('sphere2', 4, 2, scene);
+var sphere2 = BABYLON.MeshBuilder.CreateSphere('sphere2', {segments:4, diameter:2}, scene);
 sphere2.position.y = 3;
-sphere2.position.x = 3;
+sphere2.position.x = 2;
 sphere2.convertToFlatShadedMesh();
-var sphere3 = BABYLON.Mesh.CreateSphere('sphere3', 8, 2, scene);
+var sphere3 = BABYLON.MeshBuilder.CreateSphere('sphere3', {segments:8, diameter:2}, scene);
 sphere3.position.y = 3;
-sphere3.position.x = 6;
+sphere3.position.x = 4;
 sphere3.convertToFlatShadedMesh();
-var sphere4 = BABYLON.Mesh.CreateSphere('sphere4', 16, 2, scene);
+var sphere4 = BABYLON.MeshBuilder.CreateSphere('sphere4', {segments:16, diameter:2}, scene);
 sphere4.position.y = 3;
-sphere4.position.x = 9;
+sphere4.position.x = 6;
 sphere4.convertToFlatShadedMesh();
 
-var ground = BABYLON.Mesh.CreateGround('ground1', 60, 60, 4, scene);
+var lathe = BABYLON.MeshBuilder.CreateLathe('lathe', {shape: [
+  new BABYLON.Vector3(0,0,0),
+  new BABYLON.Vector3(1,.1,0),
+  new BABYLON.Vector3(.5,.3,0),
+  new BABYLON.Vector3(.3,.7,0),
+  new BABYLON.Vector3(.7,1,0),
+  new BABYLON.Vector3(0,2,0),
+]})
+lathe.position.y = 2;
+lathe.position.x = -2;
+
+var ground = BABYLON.MeshBuilder.CreateGround('ground1',
+  {width:60, height:60, subdivisions:4}, scene);
 ground.material = new BABYLON.StandardMaterial('grass', scene);
 ground.material.diffuseTexture = new BABYLON.Texture('./img/grass.png', scene);
 ground.material.diffuseTexture.uScale = 10.0;
 ground.material.diffuseTexture.vScale = 10.0;
 ground.material.specularColor = new BABYLON.Color3(0,0,0);
+
+sphere4.material = new BABYLON.StandardMaterial('rainbow', scene);
+sphere4.material.diffuseTexture = new BABYLON.Texture('./img/rainbow.jpg', scene);
+
+var animationZ = new BABYLON.Animation('a1', 'rotation.z', 30,
+  BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE);
+var animationY = new BABYLON.Animation('a2', 'position.y', 30,
+  BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+var animationX = new BABYLON.Animation('a3', 'scaling.x', 30,
+  BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+var keys = [{ frame:   0, value:  0*Math.PI/8 }];
+keys.push(  { frame:  25, value:  4*Math.PI/8 });
+keys.push(  { frame:  75, value: 12*Math.PI/8 });
+keys.push(  { frame: 100, value: 16*Math.PI/8 });
+animationZ.setKeys(keys)
+keys = [{   frame:   0, value:  3 }];
+keys.push({ frame:  50, value:  6 });
+keys.push({ frame: 100, value:  3 });
+animationY.setKeys(keys)
+var ease = new BABYLON.SineEase();
+ease.setEasingMode(BABYLON.EasingFunction.EASINGmODE_EASEINOUT);
+animationY.setEasingFunction(ease);
+keys = [{   frame:   0, value: .0 }];
+keys.push({ frame:  25, value:  1 });
+keys.push({ frame:  75, value:  1 });
+keys.push({ frame: 100, value: .0 });
+animationX.setKeys(keys)
+sphere4.animations = [ animationY, animationZ ];
+scene.beginAnimation(sphere4, 0, 100, true);
 
 //render the scene and post-setup
 window.addEventListener('DOMContentLoaded', function() {
