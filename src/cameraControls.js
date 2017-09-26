80014785d0f52c0fd3830e3abe09b283c34647e3
bohadi <bohadi@users.noreply.github.com>
2017-09-25 22:21:26 -0500

    towards fixing jumpable bug

--- src/cameraControls.js
+++ src/cameraControls.js
@@ -46,15 +46,18 @@ var _initPointerLock = function(canvas, camera) {
   document.addEventListener("webkitpointerLockChange", pointerLockChange, false);
 }
 
+//TODO this creates a new animation each jump...
+//TODO it does not feel smooth
 var _jump = function (scene, camera) {
   var animation = new BABYLON.Animation("jump", "position.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
   var keys = [
     { frame:  0, value: camera.position.y },
-    { frame: 30, value: camera.position.y + 2 }
+    { frame: 15, value: camera.position.y + 3 },
+    { frame: 30, value: camera.position.y }
   ];
   animation.setKeys(keys);
-  var easefn = new BABYLON.QuadraticEase();
-  easefn.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEOUT);
+  var easefn = new BABYLON.SineEase();
+  easefn.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
   animation.setEasingFunction(easefn);
   camera.animations.push(animation);
   scene.beginAnimation(camera, 0, 30, false, 2);
@@ -71,9 +74,9 @@ var _initControlsKBM = function (scene, camera) {
     new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function (evt) {
       switch (evt.sourceEvent.keyCode) {
         case 32: //spacebar jump
+          console.log(p1.canJumpAgain);
           if (p1.canJumpAgain) {
             p1.canJumpAgain = false;
-            console.log(1)
             _jump(scene, camera);
           }
           break;
@@ -121,12 +124,12 @@ var _initControls = function (scene, camera) {
   camera.angularSensibility = 750.0;  //default 2000, lower faster
   camera.onCollide = function (collidedMesh) {
     if (collidedMesh.jumpable) {
-      p1.canJumpAgain = true;
+      //p1.canJumpAgain = true;
   }}
 }
 
 var _initCollisionGravity = function (scene, camera) {
-  scene.gravity = new BABYLON.Vector3(0, -0.2, 0); //-9.81
+  scene.gravity = new BABYLON.Vector3(0, -0.3, 0); //-9.81
   camera.applyGravity = true;
   camera.ellipsoid = new BABYLON.Vector3(1, 2, 1); //default 0.5, 1, 0.5
   camera.position.y = 4; // match collision ellipsoid
