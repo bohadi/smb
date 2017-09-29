be0c7c6f8f224acc0eb9aee980fdff206bdfd450
bohadi <bohadi@users.noreply.github.com>
2017-09-29 01:27:46 -0500

    imrpove shadows; build bundle

--- src/scene.js
+++ src/scene.js
@@ -4,6 +4,8 @@
 //const BABYLON = require('../cdn/babylon.js');
 //const STATS = require('../cdn/stats.min.js');
 
+const UTIL = require('./util.js');
+
 const CC = require('./cameraControls.js');
 const PL = require('./pointerLock.js');
 
@@ -67,10 +69,19 @@ document.addEventListener("keydown", function (evt) {
 
 //lighting
 var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-light.intensity = 0.75;
+light.intensity = 0.2;
 var spotlight = new BABYLON.SpotLight('spotlight',
-  new BABYLON.Vector3(18, 14, 13), new BABYLON.Vector3(-.5, -1, -0.3), 1.2, 2, scene);
-spotlight.diffuse = new BABYLON.Color3(1, 1, 1);
+  new BABYLON.Vector3(19, 17, 15), new BABYLON.Vector3(-.6, -1, -.3), 1.4, 1, scene);
+spotlight.diffuse = new BABYLON.Color3(.3,.3,.3).add(BABYLON.Color3.Random());
+var lamp = new BABYLON.MeshBuilder.CreateCylinder('lamp', {diameter:5}, scene);
+lamp.material = new BABYLON.StandardMaterial('lampcolor', scene);
+lamp.material.diffuseColor = spotlight.diffuse.add(BABYLON.Color3.Gray());
+lamp.position.x = 20;
+lamp.position.y = 16;
+lamp.position.z = 15;
+lamp.rotation.x = UTIL.deg2rad(64);
+lamp.rotation.y = UTIL.deg2rad(57);
+//lamp.rotation.z = UTIL.deg2rad(15);
 
 //TODO volumetric light 'godrays'
 
@@ -154,6 +165,10 @@ platform1.checkCollisions  = true;
 platform2.checkCollisions  = true;
 platform3.checkCollisions  = true;
 fountain.checkCollisions = true;
+p1.body.head.checkCollisions = true;
+p1.body.chest.checkCollisions = true;
+p1.body.lFoot.checkCollisions = true;
+p1.body.rFoot.checkCollisions = true;
 
 //shadows
 var shadowGenerator = new BABYLON.ShadowGenerator(512, spotlight);
@@ -164,6 +179,12 @@ shadowGenerator.getShadowMap().renderList.push(sphere4);
 shadowGenerator.getShadowMap().renderList.push(sphere3);
 shadowGenerator.getShadowMap().renderList.push(sphere2);
 shadowGenerator.getShadowMap().renderList.push(sphere1);
+shadowGenerator.getShadowMap().renderList.push(fountain);
+shadowGenerator.getShadowMap().renderList.push(p1.body.head);
+shadowGenerator.getShadowMap().renderList.push(p1.body.chest);
+shadowGenerator.getShadowMap().renderList.push(p1.body.lFoot);
+shadowGenerator.getShadowMap().renderList.push(p1.body.rFoot);
+shadowGenerator.getShadowMap().renderList.push(p1.equip.lHand);
 platform1.receiveShadows = true;
 platform2.receiveShadows = true;
 platform3.receiveShadows = true;
@@ -171,7 +192,24 @@ sphere1.receiveShadows = true;
 sphere2.receiveShadows = true;
 sphere3.receiveShadows = true;
 sphere4.receiveShadows = true;
+fountain.receiveShadows = true;
+p1.body.head.receiveShadows = true;
+p1.body.chest.receiveShadows = true;
+p1.body.lFoot.receiveShadows = true;
+p1.body.rFoot.receiveShadows = true;
+p1.equip.lHand.receiveShadows = true;
 ground.receiveShadows = true;
+//shadowGenerator.forceBackFacesOnly = true;
+//soften shadows
+//shadowGenerator.usePoissonSampling = true;
+//shadowGenerator.useBlurExponentialShadowMap = true;
+shadowGenerator.useKernelBlur = true;
+shadowGenerator.blurKernel = 32;
+//shadowGenerator.useCloseExponentialShadowMap = true;
+shadowGenerator.useBlurCloseExponentialShadowMap = true;
+spotlight.shadowMinZ= 10;
+spotlight.shadowMaxZ= 30;
+//shadowGenerator.frustumEdgeFalloff = 1.0;
 
 //materials
 ground.material = new BABYLON.StandardMaterial('grass', scene);
@@ -260,7 +298,7 @@ var shrubtreeSpriteManager =
 var tree = new BABYLON.Sprite('tree', shrubtreeSpriteManager);
 tree.size = 20;
 tree.position.y = tree.size/2 - 1;
-tree.position.z = 25;
+tree.position.z = 27;
 for (var i = 0; i < numShrubs; i++) {
   var shrub = new BABYLON.Sprite("tree", shrubtreeSpriteManager);
   shrub.position.x =  -30 + 60*Math.random();
